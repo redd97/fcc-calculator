a40f696fa17b61c6202922415a7b94cadfbe1cba
rediska97 <Kingstoonesis@gmail.com>
2022-11-24 11:59:40 +0200

    Create calculator interface

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,8 +1,35 @@
 
 function Calculator() {
   return (
-    <div className="App">
-        Calculator app
+    <div className="calc-wrapper">
+
+      <div id='display'>123+</div>
+
+      <div className="buttons">
+        <div id="equals">=</div>
+        <div id='clear'>AC</div>
+
+
+        <div id='add'>+</div>
+        <div id='substract'>-</div>
+        <div id='multiply'>*</div>
+        <div id='devide'>/</div>
+        <div id='decimal'>.</div>
+
+        <div id="zero">0</div>
+        <div id="one">1</div>
+        <div id="two">2</div>
+        <div id="three">3</div>
+        <div id="four">4</div>
+        <div id="five">5</div>
+        <div id="six">6</div>
+        <div id="seven">7</div>
+        <div id="eight">8</div>
+        <div id="nine">9</div>
+      </div>
+
+
+
     </div>
   );
 }
